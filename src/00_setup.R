5f0d62c5b81953f4ce636c5f55a2b42e93c29c14
cristina-pozzoli <cristina.pozzoli@icloud.com>
2022-11-26 16:20:40 +0100

    Uploads the original dataset

---
+++ src/00_setup.R
@@ -0,0 +1,14 @@
+#CE 1
+
+#Team Filippone-Pozzoli
+
+#This script is the set up for the data analysis of the project:
+
+# load the package----------------------------------------------
+
+library(rio)
+
+# importing the original dataset: ------------------------------
+
+data <- import("/Users/cristinapozzoli/Desktop/CE1_Filippone_Pozzoli/data_orig/ESS8e02_2.dta",
+               encoding = "UTF-8")
