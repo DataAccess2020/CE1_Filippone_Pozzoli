e5daee45bf20bacc68792a53bbfc539c08d02d4a
Lara Filippone <lara.filippone@studenti.unimi.it>
2022-11-26 16:57:36 +0100

    adds bar graph

---
+++ src/03_plot.R
@@ -0,0 +1,5 @@
+# Creating the plot----
+
+barplot(table(data$global_warming_attitude, data$age), beside = T, 
+        main = "Climate change Anxiety", 
+        legend.text = c("Not Worried", "Somewhat worried", "Worried" ))
