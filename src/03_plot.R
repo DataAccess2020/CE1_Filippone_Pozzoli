4729e1ffa60a6d242c836e95f078c97fde2ece3e
cristina-pozzoli <cristina.pozzoli@icloud.com>
2022-11-26 17:05:29 +0100

    Changes colours in the graph

--- src/03_plot.R
+++ src/03_plot.R
@@ -2,4 +2,5 @@
 
 barplot(table(data$global_warming_attitude, data$age), beside = T, 
         main = "Climate change Anxiety", 
-        legend.text = c("Not Worried", "Somewhat worried", "Worried" ))
+        legend.text = c("Not Worried", "Somewhat worried", "Worried" ),
+        col = c("lightblue", "mistyrose", "lightcyan"))
